bd191bf788e862e5f5899d3e4b16489869b4084a
Djonahuti <86109337+Djonahuti@users.noreply.github.com>
2024-12-17 16:19:42 +0000

    commit

--- uhome/src/components/Education.js
+++ uhome/src/components/Education.js
@@ -1,108 +1,138 @@
-import React, { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import React from "react";
 
-function Education() {
-  const sectionRef = useRef(null);
-  const isInView = useInView(sectionRef, { once: true });
-
-  const fadeInVariant = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
-  };
-
+function Sidebar({ currentPage, setCurrentPage }) {
   return (
-    <section className="py-me" ref={sectionRef}>
-      <div className="container-fluid my-5 py-6">
-        <div className="container">
-          <motion.div
-            initial="hidden"
-            animate={isInView ? "visible" : "hidden"}
-            variants={fadeInVariant}
-          >
-            <div className="row g-5 mb-1">
-              <div className="col-lg-6 col-xs-6">
-                <p className="display-6">
-                  <i className="bi bi-mortarboard-fill text-gradient"></i> Education
-                </p>
-              </div>
-              <div className="col-lg-6 col-xs-6 mb-0">
-                <p className="display-5 mb-0"></p>
-              </div>
+    <>
+      <div id="layoutSidenav_nav">
+        <nav className="sb-sidenav accordion" id="sidenavAccordion">
+          <div className="text-center">
+            <div
+              className="author-img"
+              style={{ backgroundImage: `url(assets/biglap.jpg)` }}
+            ></div>
+            <h1 id="my-info">
+              <a href="https://www.uhome.com.ng">David Utibe Jonah</a>
+            </h1>
+            <span className="pow">
+              <a href="https://www.uhome.com.ng">WEB DEVELOPER</a><small className="dav-fs"> from Nigeria</small>
+            </span>
+          </div>
+          <div className="sb-sidenav-menu">
+            <div className="nav">
+              <ul>
+                <li>
+                  <a
+                    href="#home"
+                    onClick={() => setCurrentPage("home")}
+                    data-nav-section="home"
+                    className={currentPage === "home" ? "active-link" : ""}
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#about"
+                    onClick={() => setCurrentPage("about")}
+                    data-nav-section="about"
+                    className={currentPage === "about" ? "active-link" : ""}
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#work"
+                    onClick={() => setCurrentPage("work")}
+                    data-nav-section="work"
+                    className={currentPage === "work" ? "active-link" : ""}
+                  >
+                    Experience
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#education"
+                    onClick={() => setCurrentPage("education")}
+                    data-nav-section="education"
+                    className={currentPage === "education" ? "active-link" : ""}
+                  >
+                    Education
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#certificate"
+                    onClick={() => setCurrentPage("certificate")}
+                    data-nav-section="cerificate"
+                    className={currentPage === "certificate" ? "active-link" : ""}
+                  >
+                    Certificates
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#projects"
+                    onClick={() => setCurrentPage("projects")}
+                    data-nav-section="work"
+                    className={currentPage === "projects" ? "active-link" : ""}
+                  >
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#contact"
+                    onClick={() => setCurrentPage("contact")}
+                    data-nav-section="contact"
+                    className={currentPage === "contact" ? "active-link" : ""}
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
             </div>
-          </motion.div>
-          {/* Education Card 1 */}
-          <motion.div
-            initial="hidden"
-            animate={isInView ? "visible" : "hidden"}
-            variants={fadeInVariant}
-          >
-            <div className="card bg-white shadow border-0 rounded-4 mb-5">
-              <div className="card-body p-5">
-                <div className="row align-items-center gx-5">
-                  <div className="col text-center text-lg-start mb-4 mb-lg-0">
-                    <div className="bg-litx p-4 rounded-4">
-                      <div className="text-secondary fw-bolder mb-2">2019 - 2023</div>
-                      <div className="mb-2">
-                        <div className="small fw-bolder">
-                          National Open University Of Nigeria
-                        </div>
-                        <div className="small text-muted">Lagos Nigeria</div>
-                      </div>
-                      <div className="fst-italic">
-                        <div className="small text-muted">Bachelor's Degree</div>
-                        <div className="small text-muted">Computer Science</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-8">
-                    <div>
-                      The B.Sc. Computer Science program provided me with the fundamental
-                      knowledge and skills needed to work in various computing areas and
-                      develop a strong understanding of applied mathematics.
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-          {/* Education Card 2 */}
-          <motion.div
-            initial="hidden"
-            animate={isInView ? "visible" : "hidden"}
-            variants={fadeInVariant}
-          >
-            <div className="card bg-white shadow border-0 rounded-4 mb-5">
-              <div className="card-body p-5">
-                <div className="row align-items-center gx-5">
-                  <div className="col text-center text-lg-start mb-4 mb-lg-0">
-                    <div className="bg-litx p-4 rounded-4">
-                      <div className="text-secondary fw-bolder mb-2">2023 - 2024</div>
-                      <div className="mb-2">
-                        <div className="small fw-bolder">Coding College</div>
-                        <div className="small text-muted">Udemy</div>
-                      </div>
-                      <div className="fst-italic">
-                        <div className="small text-muted">Degree</div>
-                        <div className="small text-muted">
-                          Web Design & Development Mastery
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-8">
-                    <div>
-                      The "Web Design & Development Mastery" course was exceptionally
-                      well-structured, comprehensive, and practical.
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        </div>
+          </div>
+          <div className="sb-sidenav-footer">
+            <p>
+              <small>
+                &copy;Copyright U Home 2018 All rights reserved. Made
+                with <i className="bi bi-heart-fill" aria-hidden="true" style={{ color: "orange" }}></i> by
+                <a href="#!"> David Utibe Jonah</a>
+              </small>
+            </p>
+            <ul>
+              <li>
+                <a className="text-gradient" href="#!">
+                  <i className="bi bi-twitter-x"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-gradient"
+                  href="https://github.com/Djonahuti"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bi bi-github"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-gradient"
+                  href="https://www.linkedin.com/in/david-utibe-jonah-482210234/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </section>
+    </>
   );
 }
 
-export default Education;
+export default Sidebar;
