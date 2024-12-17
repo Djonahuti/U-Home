8e8fd3a501d15b3f9b7e2a085cdb37263168c605
Djonahuti <86109337+Djonahuti@users.noreply.github.com>
2024-12-17 14:47:14 +0000

    commit

--- uhome/src/utils/data.js
+++
@@ -1,6 +0,0 @@
-export const projects = [
-    { id: 1, title: "Project One", description: "Description for project one." },
-    { id: 2, title: "Project Two", description: "Description for project two." },
-    // add more projects here
-  ];
-  
